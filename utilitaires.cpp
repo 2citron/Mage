7c98e0e84f98789318b8ac47ab8609d6a1c5bd11
Samuel Lambert <lambert.samuel@carrefour.cegepvicto.ca>
2021-02-11 16:02:45 -0500

    push probleme include 2

--- utilitaires.cpp
+++ utilitaires.cpp
@@ -4,7 +4,7 @@ int getRng(int min, int max)
 {
 	std::random_device rd;
 	std::mt19937 rng(rd());
-	// dist1 sert à normaliser le nombre généré entre 1 et 100
+	// dist1 sert à normaliser le nombre généré entre 1 et 101
 	std::uniform_int_distribution<> dist(min, max);
 	return dist(rng);
 }
